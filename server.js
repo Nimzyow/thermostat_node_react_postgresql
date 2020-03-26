a415b4e6ae425484f712d336f5215728bd89501d
Nimzyow <n_soufiani@hotmail.com>
2020-03-26 00:06:04 +0000

    succesfully truncated table

--- server.js
+++ server.js
@@ -1,17 +1,28 @@
 const express = require("express");
 const cors = require("cors");
 const Sequelize = require("./db");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 app.use(cors());
+//body parser middleware to see whats inside the body of the post request
+app.use(bodyParser());
 
 app.get("/", (req, res) => {
   res.json({ msg: "hello world!" });
 });
 
-app.post("/save", (req, res) => {
-  res.json({ msg: "I got to the post request" });
+app.post("/save", async (req, res) => {
+  try {
+    await Sequelize.query("DELETE FROM thermo;");
+    const [results, metadata] = await Sequelize.query(
+      `INSERT INTO thermo (temperature, city) VALUES ('${req.body.temperature}', '${req.body.city}');`
+    );
+    res.json({ msg: "Inserted into thermo table" });
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 module.exports = app;
